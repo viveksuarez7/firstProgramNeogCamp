e8a4bbf7567ff6e03809de659cb7106e02fef153
viveksuarez7 <46070223+viveksuarez7@users.noreply.github.com>
2021-08-29 08:41:59 +0000

    Added a welcome Message

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 console.log('Vivek Singh');
 var readlineSync = require('readline-sync');
-var username = readlineSync.question('May I have your name ?');
-console.log('Hi ' + username + '!');
+var username = readlineSync.question('May I have your name ? ');
+console.log( 'Welcome ' + username + '!');
