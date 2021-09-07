0a8cb5c0f3015c6dbdf3bab0106eb0f65b737b5f
viveksuarez7 <46070223+viveksuarez7@users.noreply.github.com>
2021-09-07 19:19:26 +0000

    Update:Added code for Football game App

--- index.js
+++ index.js
@@ -1,4 +1,63 @@
-console.log('Vivek Singh');
+//Objects
+
 var readlineSync = require('readline-sync');
-var username = readlineSync.question('May I have your name ? ');
-console.log( 'Welcome ' + username + '!');
+var score = 0;
+var userName = readlineSync.question('Hi what is your name ? ');
+console.log('Hi ' + userName + ' lets have a football quiz !');
+
+function play(question,answer){
+  var userAns = readlineSync.question(question);
+  if(userAns===answer){
+    console.log('You are correct');
+    score++;
+    console.log('Your Score is '+ score);
+    console.log('----------------------------')
+  }
+  else{
+    console.log('No you are wrong');
+    score--;
+    console.log('Your score is '+score);
+    console.log('----------------------------')
+  }
+}
+
+
+question1={
+  question:'Most Ucl trophies won by club ? ',
+  answer:'Real Madrid'
+}
+
+question2={
+  question:'Most Ucl trophies won by a English club ? ',
+  answer:'Liverpool'
+}
+question3={
+  question:'Who is the Goat ? ',
+  answer:'Messi'
+}
+
+var highScore = [
+  {
+    name:'Vivek',
+    score:3
+  },
+  {
+    name:'Aadi',
+    score:2
+
+  }
+]
+
+var questionArr = [question1,question2,question3];
+
+for(var i=0;i<questionArr.length;i++){
+  play(questionArr[i].question,questionArr[i].answer)
+}
+
+console.log('You win your overall Score is ' + score);
+
+
+console.log('High Scores are : ');
+for(var i=0;i<highScore.length;i++){
+  console.log(highScore[i].name,highScore[i].score);
+}
